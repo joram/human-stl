0bf2595e37a2761dddb82e6a9c3184b555b860fc
John Oram <john@intlabs.io>
2024-04-04 12:41:59 -0700

    photogrammetry

---
+++ openMVS/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+#python3 /openMVS/scripts/python/MvgMvsPipeline.py /data/input /data/work
+find . -name openMVG_main_SfMInit_ImageListing
+bash
---
+++ photogrammetry/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python3 /pipeline.py /data/input /data/work
+bash
